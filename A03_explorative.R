1e77e272397b4f750cb35d28b5e57d45a30653c8
Philipp Thienel <philipp.thienel@gmail.com>
2015-11-30 01:32:27 +0100

    renamed to A03_explorative.R

--- A03_explorative.R
+++ A03_explorative.R
@@ -96,16 +96,7 @@ with(data, table(abmfk))
 
 
 
-
-
-
-
-ggplot(data, aes(x=age,y=preis)) + geom_point() + geom_smooth(method="lm", se=FALSE, fullrange=TRUE)
-plt <- ggplot(data, aes(x=age,y=preis, colour=is.na(energieeffizienz))) + geom_point(alpha=0.5)
-plt
-plt + geom_smooth(method="lm", se=FALSE, fullrange=TRUE)
-
-
+library(dplyr)
 df <- filter(data, !(plattform %in% c("LT","Multivan","VAR","Variant")))
 df$plattform <- factor(df$plattform)
 
@@ -117,16 +108,6 @@ plt
 
 plt <- ggplot(df, aes(x=age)) + geom_histogram() + facet_grid(.~plattform)
 plt
-
-plt <- ggplot(df, aes(x=preis, fill = as.factor(round(age/12)))) + geom_histogram(alpha=0.7)
-plt
-
-
-
-
-
-index <- with(df, which(energieeffizienz %in% c("A","B","C","D") & age<=36))
-fit <- lm(preis ~ kilometer + log(age/12+1) + dummyEnergie, df)
 
 
 
